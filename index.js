db383d0341cd9358db7cf24547a4c61786c6adbe
Kat <bolivarkat@yahoo.com>
2021-08-13 23:00:08 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+const employee = {
+    name : "Kat",
+    streetAddress : "1703 Paseo Encinal Dr",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployeeInfo = {...employee};
+    newEmployeeInfo[key] = value;
+    return newEmployeeInfo;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newDeleted = {...employee};
+    delete newDeleted[key];
+    return newDeleted;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
